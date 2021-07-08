bf8bf558174b39062ed174008f9a3b8187f37a57
Gabriel Fernandes <72037743+BeyondBirthd4y@users.noreply.github.com>
2021-07-07 21:56:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace maiorValor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor, total, media, maiorValor = 0;
+            int n = 0;
+            char continuar = 's';
+            total = 0;
+            while (continuar != 'n')
+            {
+                Console.Write("Insira um valor:");
+                valor = double.Parse(Console.ReadLine());
+                if (maiorValor < valor)
+                {
+                    maiorValor = valor;
+                }
+                n++;
+                total += valor;
+                Console.WriteLine("Deseja continuar ?(s/n)");
+                continuar = char.Parse(Console.ReadLine());
+            }
+            media = total / n;
+            Console.WriteLine("total = {0}", total);
+            Console.WriteLine("media = {0}", media);
+            Console.WriteLine("maior valor:"+ maiorValor);
+        }
+    }
+}
